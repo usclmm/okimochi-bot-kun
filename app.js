4697bc841f74d2dcdb19b9b92b04cc308745d6a8
usclmm <usclmm.wip@gmail.com>
2022-09-04 19:58:24 +0900

    botの投稿についたリアクションに応じたレスポンスを返却

--- app.js
+++ app.js
@@ -9,8 +9,38 @@ const app = new App({
     appToken: process.env.SLACK_APP_TOKEN
 });
 
-app.message('hello',async({message,say})=>{
-    await say(`Hey there <@${message.user}>!`);
+app.event('app_mention', ({ say }) => {
+        say('こんにちは！現在のお気持ちはいかがですか？\n:hugging_face::wink::neutral_face::thinking_face::pleading_face::innocent::rage:');
+});
+
+app.event('reaction_added',async({event, say}) => {
+    const bot_id = process.env.BOT_ID;
+    if (bot_id===event.item_user){
+        switch(event.reaction){
+            case 'hugging_face':
+                await say(`<@${event.user}>さんは元気いっぱいです。:hugging_face:`);
+                break;
+            case 'wink':
+                await say(`<@${event.user}>さんは調子が良いようです。:wink:`);
+                break;
+            case 'neutral_face':
+                await say(`<@${event.user}>さんは普通の顔をしています。:neutral_face:`);
+                break;
+            case 'thinking_face':
+                await say(`<@${event.user}>さんは考えています...:thinking_face:`)
+            case 'pleading_face':
+                await say(`<@${event.user}>さんはぴえんな気持ちのようです。:pleading_face:`);
+                break;
+            case 'innocent':
+                await say(`<@${event.user}>さんは天使の笑顔を浮かべています。:innocent:`)
+                break;
+            case 'rage':
+                await say(`<@${event.user}>さんが怒りました！:rage:`)
+            case 'dusty_stick':
+                await say(`<@${event.user}>さんは汚い棒を振りかざしています。:dusty_stick:`);
+                break;   
+        }
+    }
 });
 
 (async() =>{
